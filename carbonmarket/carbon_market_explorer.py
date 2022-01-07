d80b8ae879b47029856f6e754119724b353f77d0
Bertill Browne <bertillbrowne@gmail.com>
2022-01-07 04:44:04 -0400

    First commit

---
+++ carbonmarket/carbon_market_explorer.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+
+st.title('Carbon Markets Explorer') 
+st.subheader('Exploratory analysis of carbon markets') 
+#st.write('This app carries out exploratory analysis of the Shenzhen, Shanghai, Beijing, Guangdong, Tianjin, Hubei, Chongqing and, Fujian carbon trading markets') 
